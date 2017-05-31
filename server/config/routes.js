e492d8b3b5f8aeb96b501abd2f94e0a9af723a3c
Tom Bridgeman <TBridgeman78@gmail.com>
2017-05-31 11:00:23 -0700

    mailgun setting

--- server/config/routes.js
+++ server/config/routes.js
@@ -25,11 +25,11 @@ module.exports = function(app){
 	app.post('/contact', function(req,res){
 
 		var api_key = 'key-2451a2b90a87be616ab68b8f7c8f97ea';
-		var domain = 'sandbox7dedeb0d5d384b6a8ce4f49165204257.mailgun.org';
+		var domain = 'postmaster@tombridgeman.com';
 		var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 		 
 		var data = {
-		  from: 'Website inquiry <postmaster@sandbox7dedeb0d5d384b6a8ce4f49165204257.mailgun.org>',
+		  from: 'Website inquiry <postmaster@tombridgeman.com>',
 		  to: 'tombridgemandev@gmail.com',
 		  subject: req.body.full_name+" has sent you a message",
 		  html:
